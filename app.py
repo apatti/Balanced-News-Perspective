f94c9c86b3dfaf213c4c28c368db9c3af74d78eb
Ashwin Patti <ashwin.patti@gmail.com>
2025-02-26 01:48:02 -0800

    name typo

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from utils.messageFormat import MessageFormat
 import dotenv
 dotenv.load_dotenv()
 
-st.title("Balanced News Perpective")
+st.title("Balanced News Perspective")
 st.write("An innovative tool designed to provide users with diverse perspectives on news headlines.")
 st.write(" The app takes a headline as input and delivers three distinct views: right-leaning, left-leaning, and center/unbiased. By presenting these perspectives, the app ensures users gain a well-rounded understanding of the news, fostering critical thinking and informed discussions.")
 headLine = st.text_input("Enter a news headline:")
